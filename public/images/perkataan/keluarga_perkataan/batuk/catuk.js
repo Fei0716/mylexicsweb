9f942ac9697864e00e5da2daf66218e731a57e53
Fei0716 <ahfeiahfeiahfei@gmail.com>
2025-07-26 03:08:09 +0800

    Added assets for keluarga perkataan

---
+++ public/images/perkataan/keluarga_perkataan/batuk/catuk.js
@@ -0,0 +1,14 @@
+﻿(function(window) {
+catuk_instance_1 = function() {
+	this.initialize();
+}
+catuk_instance_1._SpriteSheet = new createjs.SpriteSheet({images: ["catuk.png"], frames: [[0,0,231,133,0,80.55,48.8],[231,0,231,135,0,80.55,50.8],[462,0,231,135,0,80.55,50.8],[693,0,231,133,0,80.55,48.8],[0,135,231,130,0,80.55,45.8],[231,135,231,130,0,80.55,45.8],[462,135,231,133,0,80.55,48.8],[693,135,231,135,0,80.55,50.8],[0,270,231,135,0,80.55,50.8],[231,270,231,133,0,80.55,48.8]]});
+var catuk_instance_1_p = catuk_instance_1.prototype = new createjs.Sprite();
+catuk_instance_1_p.Sprite_initialize = catuk_instance_1_p.initialize;
+catuk_instance_1_p.initialize = function() {
+	this.Sprite_initialize(catuk_instance_1._SpriteSheet);
+	this.paused = false;
+}
+window.catuk_instance_1 = catuk_instance_1;
+}(window));
+
---
+++ public/images/perkataan/keluarga_perkataan/batuk/cucuk.js
@@ -0,0 +1,14 @@
+﻿(function(window) {
+cucuk_instance_1 = function() {
+	this.initialize();
+}
+cucuk_instance_1._SpriteSheet = new createjs.SpriteSheet({images: ["cucuk.png"], frames: [[0,0,180,174,0,62.3,74.7],[180,0,176,174,0,58.3,74.7],[356,0,172,174,0,54.3,74.7],[528,0,168,174,0,50.3,74.7],[696,0,164,174,0,46.3,74.7],[860,0,160,174,0,42.3,74.7],[0,174,155,174,0,37.3,74.7],[155,174,151,174,0,33.3,74.7],[155,174,151,174,0,33.3,74.7],[155,174,151,174,0,33.3,74.7],[155,174,151,174,0,33.3,74.7],[306,174,179,186,0,33.3,86.7],[306,174,179,186,0,33.3,86.7],[485,174,187,193,0,41.3,90.7],[485,174,187,193,0,41.3,90.7],[672,174,180,114,0,48.3,8.700000000000003],[0,367,194,114,0,62.3,8.700000000000003]]});
+var cucuk_instance_1_p = cucuk_instance_1.prototype = new createjs.Sprite();
+cucuk_instance_1_p.Sprite_initialize = cucuk_instance_1_p.initialize;
+cucuk_instance_1_p.initialize = function() {
+	this.Sprite_initialize(cucuk_instance_1._SpriteSheet);
+	this.paused = false;
+}
+window.cucuk_instance_1 = cucuk_instance_1;
+}(window));
+
